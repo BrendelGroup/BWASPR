67f94a10e2f0cee5b15fa2606e18d6a82e363e20
Volker Brendel <vbrendel@indiana.edu>
2017-03-03 21:03:36 -0500

    updated dependencies

--- R/mcalls2mkobj.R
+++ R/mcalls2mkobj.R
@@ -20,9 +20,10 @@
 #' @return A methylKit-package methylRaw or methylRawList object.
 #'
 #' @importFrom methylKit methRead
+#' @importFrom utils write.table
 #'
 #' @examples
-#'   library("methylKit")
+#    library("methylKit")
 #'   mydatf <- system.file("extdata","Am.dat",package="BWASPR")
 #'   myparf <- system.file("extdata","Am.par",package="BWASPR")
 #'   myfiles <- setup_BWASPR(datafile=mydatf,parfile=myparf)
