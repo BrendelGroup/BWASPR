5eaa6e2502a1b1f8422b2a93e682555a8c09241e
ShengyaoChen <chenshengyaok2@gmail.com>
2017-03-28 22:07:57 -0400

    fixed some coding issues

--- R/temp_plot_diff_genes_pattern.R
+++ R/temp_plot_diff_genes_pattern.R
@@ -13,13 +13,9 @@
 #'   myfiles <- setup_BWASPR(datafile=mydatf,parfile=myparf)
 #'   AmHE <- mcalls2mkobj(myfiles$datafiles,species="Am",study="HE",type="CpGhsm",
 #'                        mincvrg=1,assembly="Amel-4.5")
-<<<<<<< HEAD
-#'   ginfo <- get_genome_annotation(myfiles$parameters)
-#'   meth_diff <- get_diff_genes(AmHE, ginfo)
-=======
+
 #'   genome <- get_genome_annotation(myfiles$parameters)
 #'   meth_diff <- get_diff_genes(AmHE, genome)
->>>>>>> upstream/master
 #'   plot_diff_genes_pattern <- function(AmHE, meth_diff)
 #'
 #' @export
@@ -29,38 +25,38 @@ plot_diff_genes_pattern <- function(mrobj, diff_genes, output_figure = 'meth_dif
 
     # calculate the percentageCs
     for (i in seq_along(sample_list)){
-        mrobj[[i]] <- mrobj[[i]] %>% mutate(percentageCs = round(numCs / coverage, digits = 2))
-        mrobj[[i]] <- as(mrobj[[i]], 'GRanges')
-        mrobj[[i]] <- subsetByOverlaps(mrobj[[i]], diff_genes$diff_sites)
+        mrobj[[i]] <- mrobj[[i]] %>% mutate(percentageCs = round(numCs / coverage,digits=2))
+        mrobj[[i]] <- as(mrobj[[i]],'GRanges')
+        mrobj[[i]] <- subsetByOverlaps(mrobj[[i]],diff_genes$diff_sites)
     }
 
     targets = GRangesList(mrobj)
     print(targets)
     # resize the diff_genes (GRanges)
-    methdiffgenes_resize.gr <- resize(diff_genes$diff_genes, 20000)
+    methdiffgenes_resize.gr <- resize(diff_genes$diff_genes,20000)
 
     test <- ScoreMatrixList(targets,
                             methdiffgenes_resize.gr,
-                            bin.num = 20,
+                            bin.num=20,
                             # using differnt bin.op could be interesting
-                            bin.op = 'mean',
-                            strand.aware = FALSE,
-                            weight.col ='percentageCs',
-                            is.noCovNA = TRUE
+                            bin.op='mean',
+                            strand.aware=FALSE,
+                            weight.col='percentageCs',
+                            is.noCovNA=TRUE
                            )
 
-    test.sub <- intersectScoreMatrixList(test, reorder = FALSE)
+    test.sub <- intersectScoreMatrixList(test,reorder=FALSE)
 
     # plot the figure
     pdf(output_figure)
     #options(repr.plot.width = 8, repr.plot.height = 5)
     multiHeatMatrix(test.sub,
-                    col = topo.colors(10),
+                    col=topo.colors(10),
                     xcoords=c(0, 20000),
                     matrix.main=names(targets),
-                    common.scale = TRUE,
-                    xlab = sample_list,
-                    legend = TRUE
+                    common.scale=TRUE,
+                    xlab=sample_list,
+                    legend=TRUE
                    )
 	#dev.off()
 
