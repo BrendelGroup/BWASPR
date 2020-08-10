374589f028a39b32a0e0df99514924d8a2afbfbb
Volker Brendel <vbrendel@indiana.edu>
2020-08-10 09:11:00 -0400

    updates to what gets plotted by rank_rbm()

--- R/show_dmsg.R
+++ R/show_dmsg.R
@@ -112,7 +112,8 @@ show_dmsg <- function(mrobj,dmsg,destrand=FALSE,min.nsites=2,max.nsites=60,
         lapply(out,function(g) {
             nsites <- dim(g)[1]
             pdmsites <- 100 * sum(g$is.dm,na.rm=TRUE) / nsites
-            if (nsites >= min.nsites  &  nsites <= max.nsites  &  pdmsites >= min.pdmsites) {
+            if (nsites >= min.nsites  &&  nsites <= max.nsites  &&
+                pdmsites >= min.pdmsites) {
                 plot <- as.matrix(g[,c(sample1,sample2)])
                 # ... making sure that there are differences to show in the heatmap:
                 if (nrow(plot) >= 2  &&  !all(plot[,1] == plot[,2])) {
